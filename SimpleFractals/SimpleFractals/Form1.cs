07707f117e5f032d2a22c5c5c5cdef096e0c606c
Brendan Bennett <bennett.brendan@yahoo.co.uk>
2016-05-21 16:43:19 +0100

    Added useful methods
    Added DrawDynamicLine method taking start point, angle and length and
    producing start and end point.
    Added DrawPoly method taking side length, number of sides and drawing
    angle offset.

---
+++ SimpleFractals/SimpleFractals/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFractals
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private PointF DrawDynamicLine(Graphics g, Pen pen, PointF point1, double angle, float length)
+        {
+            double angleRad = (angle - 90) * (Math.PI / 180);
+            PointF point2 = new PointF(Convert.ToSingle(point1.X + Math.Cos(angleRad) * length), Convert.ToSingle(point1.Y + Math.Sin(angleRad) * length));
+
+            g.DrawLine(pen, point1, point2);
+            return point2;
+        }
+
+        private void DrawPoly(Graphics g, Pen pen, PointF start, double angleOffset, float sideLength, int numberOfSides)
+        {
+            PointF point = start;
+            for (int i = 0; i < numberOfSides; i++)
+            {
+                point = DrawDynamicLine(g, pen, point, 90 + angleOffset - ((360 / numberOfSides)*i), sideLength);
+            }
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g;
+            g = this.CreateGraphics();
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Pen pen = new Pen(Color.Red, 1);
+            PointF start = new PointF(100,100);
+
+            /*
+            for (int i = 100; i > 0; i -= 5)
+            {
+                start = DrawDynamicLine(g, pen, start, i*5, i);
+            }
+            */
+
+            DrawPoly(g, pen, start, 0, 100, 3);
+        }
+    }
+}
